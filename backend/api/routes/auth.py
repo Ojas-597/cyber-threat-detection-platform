1951d2f5cf19b62f23badc58c0eeb9492aca0b9e
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-16 06:58:54 +0530

    Update auth.py

--- backend/api/routes/auth.py
+++ backend/api/routes/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from backend.authentication.jwt_handler import create_access_token
+from authentication.jwt_handler import create_access_token
 
 router = APIRouter(prefix="/auth",
                   tags=["Authentication"])
