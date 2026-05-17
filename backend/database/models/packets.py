030f71046b1de6f4ad58de5c9035ea8c0b14137e
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-17 14:25:28 +0530

    Update packets.py

--- backend/database/models/packets.py
+++ backend/database/models/packets.py
@@ -1,7 +1,48 @@
-from pydantic import BaseModel
+from datetime import datetime
 
-class Packet(BaseModel):
-    source_ip: str
-    destination_ip: str
-    protocol: str
-    packet_size: int
+from sqlalchemy import (
+    Column,
+    String,
+    Integer,
+    DateTime
+)
+
+from backend.database.base import Base
+
+
+class Packet(Base):
+    __tablename__ = "packets"
+
+    packet_id = Column(
+        String,
+        primary_key=True,
+        index=True
+    )
+
+    source_ip = Column(
+        String,
+        nullable=False
+    )
+
+    destination_ip = Column(
+        String,
+        nullable=False
+    )
+
+    protocol = Column(
+        String,
+        nullable=False
+    )
+
+    packet_size = Column(
+        Integer
+    )
+
+    risk_score = Column(
+        Integer
+    )
+
+    captured_at = Column(
+        DateTime,
+        default=datetime.utcnow
+    )
