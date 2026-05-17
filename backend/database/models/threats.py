a1e24a13bef9fe42261cefd4212cefa5b79c5f64
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-17 14:25:56 +0530

    Update threats.py

--- backend/database/models/threats.py
+++ backend/database/models/threats.py
@@ -1,7 +1,63 @@
-from pydantic import BaseModel
+from datetime import datetime
 
-class Threat(BaseModel):
-    attack_type: str
-    severity: str
-    source_ip: str
-    destination_ip: str
+from sqlalchemy import (
+    Column,
+    String,
+    Float,
+    DateTime
+)
+
+from backend.database.base import Base
+
+
+class Threat(Base):
+    __tablename__ = "threats"
+
+    threat_id = Column(
+        String,
+        primary_key=True,
+        index=True
+    )
+
+    type = Column(
+        String,
+        nullable=False
+    )
+
+    severity = Column(
+        String,
+        nullable=False
+    )
+
+    source_ip = Column(
+        String,
+        nullable=False
+    )
+
+    destination_ip = Column(
+        String,
+        nullable=False
+    )
+
+    protocol = Column(
+        String,
+        nullable=False
+    )
+
+    status = Column(
+        String,
+        nullable=False
+    )
+
+    mitre_attack_id = Column(
+        String
+    )
+
+    confidence_score = Column(
+        Float
+    )
+
+    created_at = Column(
+        DateTime,
+        default=datetime.utcnow
+    )
