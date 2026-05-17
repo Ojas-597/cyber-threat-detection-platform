51e44baafcfdf46fe369abe62eb66341f18429a6
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-17 14:20:35 +0530

    Update packets.py

--- backend/api/routes/packets.py
+++ backend/api/routes/packets.py
@@ -1,20 +1,36 @@
-from fastapi import APIRouter
-from backend.database.connection import get_db
+from fastapi import (
+    APIRouter,
+    Depends
+)
+from sqlalchemy.orm import Session
 
-router = APIRouter(prefix="/packets", tags=["Packets"])
+from backend.database.connection import (
+    get_db
+)
+from backend.database.models.packets import (
+    Packet
+)
+
+router = APIRouter(
+    prefix="/packets",
+    tags=["Packets"]
+)
+
 
 @router.get("/")
-def packets():
-    return {"message": "Packet monitoring endpoint"}
-backend/authentication/jwt_handler.py
-from jose import jwt
-from datetime import datetime, timedelta
+def packets(
+    db: Session = Depends(get_db)
+):
+    packet_records = (
+        db.query(Packet)
+        .all()
+    )
 
-SECRET_KEY = "SUPER_SECRET_KEY"
-ALGORITHM = "HS256"
-
-
-def create_access_token(data: dict):
-    payload = data.copy()
-    payload.update({"exp": datetime.utcnow() + timedelta(hours=2)})
-    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    return {
+        "message":
+            "Packet monitoring endpoint",
+        "count":
+            len(packet_records),
+        "packets":
+            packet_records
+    }
