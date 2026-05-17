58cb8b95000d7555af2840c4104a1682559dbcdf
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-17 14:20:04 +0530

    Update incidents.py

--- backend/api/routes/incidents.py
+++ backend/api/routes/incidents.py
@@ -1,8 +1,34 @@
-from fastapi import APIRouter
-from backend.database.connection import get_db
+from fastapi import (
+    APIRouter,
+    Depends
+)
+from sqlalchemy.orm import Session
 
-router = APIRouter(prefix="/incidents", tags=["Incidents"])
+from backend.database.connection import (
+    get_db
+)
+from backend.database.models.incidents import (
+    Incident
+)
+
+router = APIRouter(
+    prefix="/incidents",
+    tags=["Incidents"]
+)
+
 
 @router.get("/")
-def incidents():
-    return {"message": "Incident response endpoint"}
+def get_incidents(
+    db: Session = Depends(get_db)
+):
+    incidents = (
+        db.query(Incident)
+        .all()
+    )
+
+    return {
+        "count":
+            len(incidents),
+        "incidents":
+            incidents
+    }
