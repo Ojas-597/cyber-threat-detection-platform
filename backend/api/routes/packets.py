57e136f0024f784a739a39436bc143eb7c6f14e2
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-17 13:38:26 +0530

    Update packets.py

--- backend/api/routes/packets.py
+++ backend/api/routes/packets.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter
 
-router = APIRouter(
-    prefix="/packets",
-    tags=["Packets"]
-)
+router = APIRouter(prefix="/packets", tags=["Packets"])
 
 @router.get("/")
-def get_packets():
+def packets():
+    return {"message": "Packet monitoring endpoint"}
+backend/authentication/jwt_handler.py
+from jose import jwt
+from datetime import datetime, timedelta
 
-    return {
-        "message": "Packet monitoring endpoint"
-    }
+SECRET_KEY = "SUPER_SECRET_KEY"
+ALGORITHM = "HS256"
+
+
+def create_access_token(data: dict):
+    payload = data.copy()
+    payload.update({"exp": datetime.utcnow() + timedelta(hours=2)})
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
