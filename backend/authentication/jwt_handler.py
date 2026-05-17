2da7f6b37e66adc6ce38ca6ce0892110cf1b40ed
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-17 13:41:48 +0530

    Update jwt_handler.py

--- backend/authentication/jwt_handler.py
+++ backend/authentication/jwt_handler.py
@@ -4,35 +4,8 @@ from datetime import datetime, timedelta
 SECRET_KEY = "SUPER_SECRET_KEY"
 ALGORITHM = "HS256"
 
+
 def create_access_token(data: dict):
-
     payload = data.copy()
-
-    payload.update({
-        "exp": datetime.utcnow() + timedelta(hours=2)
-    })
-
-    token = jwt.encode(
-        payload,
-        SECRET_KEY,
-        algorithm=ALGORITHM
-    )
-
-    return token
-
-
-def verify_token(token: str):
-
-    try:
-
-        payload = jwt.decode(
-            token,
-            SECRET_KEY,
-            algorithms=[ALGORITHM]
-        )
-
-        return payload
-
-    except:
-
-        return None
+    payload.update({"exp": datetime.utcnow() + timedelta(hours=2)})
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
