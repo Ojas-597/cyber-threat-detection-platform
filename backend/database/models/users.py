64a92b303fff0c089394c62e066666a35971463d
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2026-05-17 14:26:40 +0530

    Update users.py

--- backend/database/models/users.py
+++ backend/database/models/users.py
@@ -1,7 +1,53 @@
-from pydantic import BaseModel
+from datetime import datetime
 
-class User(BaseModel):
-    username: str
-    email: str
-    password_hash: str
-    role: str
+from sqlalchemy import (
+    Column,
+    String,
+    Boolean,
+    DateTime
+)
+
+from backend.database.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(
+        String,
+        primary_key=True,
+        index=True
+    )
+
+    username = Column(
+        String,
+        unique=True,
+        nullable=False,
+        index=True
+    )
+
+    email = Column(
+        String,
+        unique=True,
+        nullable=False
+    )
+
+    password_hash = Column(
+        String,
+        nullable=False
+    )
+
+    mfa_enabled = Column(
+        Boolean,
+        default=False
+    )
+
+    is_active = Column(
+        Boolean,
+        default=True
+    )
+
+    created_at = Column(
+        DateTime,
+        default=datetime.utcnow
+    )
